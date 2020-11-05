fbb43188a32cb4f3cf0b72f539308429a7fb4b99
Ranson Knorr <ransonknorr@gmail.com>
2020-11-05 12:57:32 -0600

    finishing write the tree node class section

--- tree.py
+++ tree.py
@@ -7,11 +7,29 @@ class Node:
     @property
     def value(self):
         return self._value
-    
+
     @property
     def children(self):
         return self._children
-    
+
     def add_child(self, node):
-        if not self._children[node]: 
+        if not self._children[node]:
             self._children.append(node)
+            self._parent = self._children
+
+
+    def remove_child(self, node):
+        if node in self._children:
+            self._children.pop(node)
+            self._parent = None
+
+
+    @property
+    def parent(self):
+        return self._parent
+
+
+    @parent.setter
+    def parent(self, value):
+        self._parent = value
+        add_child(self)
