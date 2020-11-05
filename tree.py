2f009653500aefd4850a9d2ba3a047b288d247b9
john <jgh2102@gmail.com>
2020-11-06 07:14:50 +0900

    working on parent reassign

--- tree.py
+++ tree.py
@@ -21,7 +21,7 @@ class Node:
 
     def remove_child(self, node):
         if node in self._children:
-            self._children.pop(node)
+            self._children.remove(node)
             # self._parent = None
             node._parent = None
 
@@ -31,12 +31,24 @@ class Node:
 
     @parent.setter
     def parent(self, node):
-        if self._parent is node:
-            return
+        # if self._parent is node:
+        #     return
+        if self._parent is not None:
+            self._parent.remove_child(self)
         # if self._parent:
-        # node = self._parent
-        # node.remove_child(self)
+        #     node = self._parent
+        #     node.remove_child(self)
         self._parent = node
         # node.add_child(self)
         if node is not None:
             node.add_child(self)
+
+node1 = Node("root1")
+node2 = Node("root2")
+node3 = Node("root3")
+
+node3.parent = node1
+node3.parent = node2
+
+print(node1.children)
+print(node2.children)
