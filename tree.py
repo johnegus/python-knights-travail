b2b73d641fec1903a0d2e9b3fc4f6c8fc162e508
Ranson Knorr <ransonknorr@gmail.com>
2020-11-05 15:28:29 -0600

    working on parentSetter error

--- tree.py
+++ tree.py
@@ -1,5 +1,5 @@
 class Node:
-    def __init__(self, value, parent, children):
+    def __init__(self, value):
         self._value = value
         self._parent = None
         self._children = []
@@ -13,22 +13,27 @@ class Node:
         return self._children
 
     def add_child(self, node):
-        if not node in self._children:
+        if node not in self._children:
             self._children.append(node)
-
+            node._parent = self
+        if self._parent is None:
+            self._parent = node
 
     def remove_child(self, node):
         if node in self._children:
             self._children.pop(node)
-            self._parent = None
-
+            # self._parent = None
+            node._parent = None
 
     @property
     def parent(self):
         return self._parent
 
-
     @parent.setter
     def parent(self, node):
+        # if self._parent:
+        # node = self._parent
+        # node.remove_child(self)
         self._parent = node
-        node.add_child(self)
+        # node.add_child(self)
+        self._parent.add_child(self)
